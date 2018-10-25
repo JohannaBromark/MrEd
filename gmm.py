normalisation and bug fix in timbral-text

gmm.py
@@ -16,16 +16,29 @@ def read_stored_data():
 
   return features, targets
 
+def normalise(features):
+  for i in range(len(features[1])):
+    features[:,i] = np.interp(features[:,i], (features[:,i].min(), features[:,i].max()), (0, 1))
+  return features
+
 if __name__ == '__main__':
   features, targets = read_stored_data()
-  print('done')
-  # gmm = GaussianMixture()
-  # gmm.fit(features[0:5])
-  # print(gmm.predict(features[5:10]))
 
-  for i in range(30):
-    plt.subplot(6,5,i+1)
+  norm_features = normalise(features)
+
+  c = 1
+  for i in range(10):
+    plt.subplot(2,10,c)
     plt.axis('off')
-    plt.imshow(features[i][:-1].reshape(6,3))
-  plt.show()
+    plt.imshow(features[100*i][:-1].reshape(6,3))
+    c += 1
+    plt.subplot(2,10,c)
+    plt.axis('off')
+    plt.imshow(features[101*i][:-1].reshape(6,3))
+    c += 1
+  # plt.show()
+
+  gmm = GaussianMixture()
+  gmm.fit(features)
+  print(gmm.predict(features))
   

timbral-texture.py
@@ -122,8 +122,14 @@ def spectral_flux(an_wndw, prev_wndw):
   
   :param prev_wndw: The analysis window one time step prior to an_wndw
   """
-  an_wndw *= 1./np.max(an_wndw, axis=0)
-  prev_wndw *= 1./np.max(prev_wndw, axis=0)
+
+  an_wndw = np.interp(an_wndw, (an_wndw.min(), an_wndw.max()), (0, 1))
+  prev_wndw = np.interp(prev_wndw, (prev_wndw.min(), prev_wndw.max()), (0, 1))
+
+  # wrong previous normalisation
+  #an_wndw *= 1./np.max(an_wndw, axis=0) # not normalised between 0 and 1
+  #prev_wndw *= 1./np.max(prev_wndw, axis=0) # not normalised between 0 and 1
+
   return np.sum(np.power(an_wndw-prev_wndw, 2))
 
 def time_zero_crossings(wndw_no, samples, seg_size):
@@ -372,7 +378,7 @@ if __name__ == '__main__':
   #   for item in targets:
   #     file.write(str(int(item[0])))
   #     file.write('\n')
-  
+
   # gmm = GaussianMixture(n_components=3)
   # gmm.fit(featureMatrix)
   # print(gmm.predict(featureMatrix))