7c1c0ea4c3e933f3f6c8de8771358070165e3103
karlandren <andren.karl@gmail.com>
2018-10-30 13:39:25 +0100

    added stuff in knn

--- knn.py
+++ knn.py
@@ -28,14 +28,45 @@ def normalise(features):
     features[:,i] = np.interp(features[:,i], (features[:,i].min(), features[:,i].max()), (0, 1))
   return features
 
+def group_by_song(features, targets):
+  songs = []
+  grouped_targets = []
+  for i in range(len(features)//30):
+    songs.append(features[i*30:(i+1)*30])
+    grouped_targets.append(targets[i*30])
+  return np.array(songs), grouped_targets
+
+def mean_var_by_song(features, targets):
+  features_mean = np.zeros((int(len(features)//30), features.shape[1]))
+  grouped_targets = []
+  features_matrix = np.array(features)
+  for i in range(len(features)//30):
+    features_mean[i, :] = np.mean(features_matrix[i*30:(i+1)*30, :], 0)
+    # SHOULD THERE BE VARIANCE AS WELL --> 38 dimensions?
+    grouped_targets.append(targets[i*30])
+
+  return features_mean, grouped_targets
+
+def ungroup(grouped_features, grouped_targets):
+  targets_noflat = np.array([[i]*30 for i in grouped_targets])
+  targets = targets_noflat.flatten()
+
+  features_flat = grouped_features.flatten()
+  features = features_flat.reshape(-1, grouped_features.shape[2])
+
+  return features, targets
+
   
 
 if __name__ == '__main__':
 	features, targets = read_stored_data()
 	features = normalise(features)
+	
+	features_mean, grouped_targets = mean_var_by_song(features, targets)
 
+	grouped_targets = np.array(grouped_targets)
 
-	train_samples, test_samples, train_targets, test_targets = train_test_split(features, targets, test_size=0.15, random_state=42)
+	train_samples, test_samples, train_targets, test_targets = train_test_split(features_mean, grouped_targets, test_size=0.33, random_state=42)
 
 	knn = KNeighborsClassifier(n_neighbors=1)
 
