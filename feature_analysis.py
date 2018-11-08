b20bb98abb270e759500e0cb52c2f12e2421e632
Johanna <johanna.bromark@gmail.com>
2018-11-08 22:47:51 +0100

    Add some analysis docs

--- feature_analysis.py
+++ feature_analysis.py
@@ -29,43 +29,63 @@ def averageDist(dist):
 
 
 def distance_measure():
-    train_set, test_set = get_test_train_sets("features_targets/afe_feat_and_targ.txt", partition_num=0, seed=1)
+    partition_num = 6
+    seed = 1
+    for partition_num in range(10):
+        train_set, test_set = get_test_train_sets("features_targets/afe_feat_and_targ.txt", partition_num=partition_num, seed=seed)
 
-    # Normalise the data
-    train_set_norm, mean, std = normalise(train_set[:, 2:])
-    test_set_norm = (test_set[:, 2:] - mean) / std
+        # Normalise the data
+        train_set_norm, mean, std = normalise(train_set[:, 2:])
+        test_set_norm = (test_set[:, 2:] - mean) / std
 
-    dist_norm = np.zeros((len(test_set_norm), len(train_set_norm)))
+        dist_norm = np.zeros((len(test_set_norm), len(train_set_norm)))
 
-    dist_threshold = 2.0
+        dist_threshold = 2.0
 
-    for i in range(len(test_set_norm)):
-        for k in range(len(train_set_norm)):
-            dist_norm[i, k] = euclidean_dist(test_set_norm[i, :], train_set_norm[k, :])
+        for i in range(len(test_set_norm)):
+            for k in range(len(train_set_norm)):
+                dist_norm[i, k] = euclidean_dist(test_set_norm[i, :], train_set_norm[k, :])
 
-    indx_test, indx_train = np.where(dist_norm < dist_threshold)
+        indx_test, indx_train = np.where(dist_norm < dist_threshold)
 
-    unique_test, counted_test = np.unique(indx_test, return_counts=True)
-    unique_train, counted_train = np.unique(indx_train, return_counts=True)
+        unique_test, counted_test = np.unique(indx_test, return_counts=True)
+        unique_train, counted_train = np.unique(indx_train, return_counts=True)
 
-    # Pick the tracks in the training set that are close to at least 4 other tracks
-    # The indices return will map to the index in unique_train, which maps to the song
-    indx_frequent_train = np.where(counted_train > 3)
-    track_idx_train = unique_train[indx_frequent_train]
-    counted_tracks = counted_train[indx_frequent_train]
-    train_tracks_close = train_set[track_idx_train, :]
+        # Pick the tracks in the training set that are close to at least 4 other tracks
+        # The indices return will map to the index in unique_train, which maps to the song
+        indx_frequent_train = np.where(counted_train > 3)
+        track_idx_train = unique_train[indx_frequent_train]
+        counted_tracks = counted_train[indx_frequent_train]
+        train_tracks_close = train_set[track_idx_train, :]
 
-    # The test tracks that are close to the train tracks with many close train
-    test_sets_close = []
-    for i in track_idx_train:
-        idx_close_train = np.where(indx_train == i)
-        idx_tets_close = indx_test[idx_close_train]
-        test_sets_close.append([test_set[idx_tets_close, :]])
+        # The test tracks that are close to the train tracks with many close train
+        test_sets_close = []
+        for i in track_idx_train:
+            idx_close_train = np.where(indx_train == i)
+            idx_tets_close = indx_test[idx_close_train]
+            test_sets_close.append([test_set[idx_tets_close, :]])
 
+        createCsv("analysis_docs/distance_"+str(partition_num)+"("+str(seed)+").csv", train_tracks_close, test_sets_close, [seed, partition_num])
     print("help")
 
 
+def createCsv(filename, train_tracks, corresponding_test_tracks, settings):
+    with open(filename, "w") as file:
+        file.write("Random seed "+str(settings[0])+","+"Test set partition "+str(settings[1])+"\n")
+        file.write("Train track nr,"+ "Class train,"+"Test track nr,"+"Class test"+"\n")
+        for idx, train_track in enumerate(train_tracks):
+            file.write(str(int(train_track[0]))+","+str(int(train_track[1]))+"("+get_label(int(train_track[1]))+")"+"\n")
+            for test_track in corresponding_test_tracks[idx][0]:
+                file.write(","+","+str(int(test_track[0]))+","+str(int(test_track[1]))+"("+get_label(int(test_track[1]))+")"+"\n")
+            file.write("\n")
+
+
+
 def knn_neighbor_count():
+    # Checks the distance for each partition as testset and plots the result
+
+    colorDict = createColorDict()
+
     for i in range(10):
         train_set, test_set = get_test_train_sets("features_targets/afe_feat_and_targ.txt", partition_num=i, seed=1)
 
@@ -79,12 +99,11 @@ def knn_neighbor_count():
 
         test_targets = test_set[:, 1]
 
-        colorDict = createColorDict()
 
-        for i in range(len(test_set)):
-            plt.plot(i, neighbors_dist[i], "o", c=colorDict[int((test_targets[i])+1)*2])
+        # Creates a plot with all samples in the same plot
+        #for j in range(len(test_set)):
+        #    plt.plot(j, neighbors_dist[j], "o", c=colorDict[int((test_targets[i])+1)*2])
 
-    plt.show()
 
 
     print("")
@@ -113,8 +132,9 @@ def closeByTracks(dist):
 
 
 if __name__ == '__main__':
-    knn_neighbor_count()
+    #knn_neighbor_count()
     distance_measure()
+    """
     train_set, test_set = get_test_train_sets("features_targets/afe_feat_and_targ.txt")
 
     dist = []
@@ -128,7 +148,7 @@ if __name__ == '__main__':
     averageDist(dist)
     histogramish(dist)
 
-
+"""
 
 
 
